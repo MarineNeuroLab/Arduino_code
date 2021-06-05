8510399ba6b8e35e24b0e6f351ee03c4853fced5
rijacobsen <40574798+rijacobsen@users.noreply.github.com>
2021-06-05 15:09:07 +0200

    Update MeasureTemp.io to actually work
    Rewritten completely based on instructions here: https://randomnerdtutorials.com/guide-for-ds18b20-temperature-sensor-with-arduino/

--- MeasureTemp/MeasureTemp.ino
+++ MeasureTemp/MeasureTemp.ino
@@ -1,14 +1,39 @@
-const int sensorPin = A0; //Specify the pin to use
+// Measure temperatures from a waterproof DS18B20 temperature sensor
+// Values are in Celsius
+// Based on code from https://randomnerdtutorials.com/guide-for-ds18b20-temperature-sensor-with-arduino/
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+  Based on the Dallas Temperature Library example
+*********/
 
-void setup() {
-  // put your setup code here, to run once:
+// Include libraries (need to be installed first via Sketch -> Include Library -> Manage Libraries...)
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is connected to the Arduino digital pin 4
+const int oneWireBus = 4;
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to the Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // Start serial communication for debugging purposes
   Serial.begin(9600);
+  // Initialise the temperature sensor
+  sensors.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  float sensorVal = analogRead(sensorPin); //Read values from the sensor
-  Serial.print("Sensor value: "); //Print the value
-  Serial.println(sensorVal);
-  delay(100);
+void loop(void){ 
+  // Call sensors.requestTemperatures() to get the temperature of the devices on the bus
+  sensors.requestTemperatures(); 
+  
+  //Serial.print("Temp in Celsius: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.println(sensors.getTempCByIndex(0)); //Get and print the temperature in Celsius
+  delay(100); //Delay for a certain amount of time before requesting the next temperature reading
 }
