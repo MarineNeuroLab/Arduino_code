d801879419827bb60fdfe47463e7debc4ef5b6fa
rijacobsen <40574798+rijacobsen@users.noreply.github.com>
2021-10-24 19:19:03 +0200

    Fixed number of pins to use

--- LEDsON/LEDsON.ino
+++ LEDsON/LEDsON.ino
@@ -1,11 +1,11 @@
 // This sketch turns up tp 5 LEDs to max brightness and leaves them on
 
-int Pins[4] = {3,5,6,9,11}; //Specify which pins to use
+int Pins[5] = {3,5,6,9,11}; //Specify which pins to use
 //3=blue, 5=green, 6=yellow, 9=red, 11=white
 
 void setup() {
   //Loop through pins to set them as output and on
-    for(int i=0; i<4; i++){
+    for(int i=0; i<5; i++){
     pinMode(Pins[i],OUTPUT);
     digitalWrite(Pins[i],HIGH);
     }
