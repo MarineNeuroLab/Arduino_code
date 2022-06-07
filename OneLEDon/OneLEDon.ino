25cde379f23465d46209e18a04f0b7efe3a7690b
rijacobsen <40574798+rijacobsen@users.noreply.github.com>
2022-06-07 10:01:15 +0200

    Create OneLEDon.ino

---
+++ OneLEDon/OneLEDon.ino
@@ -0,0 +1,14 @@
+// This sketch turns one LED on and leaves it on
+int pin = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(pin,OUTPUT);
+    analogWrite(pin,70);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
