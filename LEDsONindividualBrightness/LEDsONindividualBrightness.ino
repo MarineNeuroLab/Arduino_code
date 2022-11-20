840bfa4ab32c698419838eaae13bb3eb5dec4d05
rijacobsen <40574798+rijacobsen@users.noreply.github.com>
2022-11-20 20:49:40 +0100

    Update LEDsONindividualBrightness.ino

--- LEDsONindividualBrightness/LEDsONindividualBrightness.ino
+++ LEDsONindividualBrightness/LEDsONindividualBrightness.ino
@@ -2,7 +2,7 @@
 
 int Pins[5] = {3,5,6,9,11}; //Specify which pins to use, 2,4,6,8
 //3=blue, 5=green, 6=yellow, 9=red, 11=white
-int brightness[5] = {255,120,65,115,85}; //Specify which brightness to use for each LED
+int brightness[5] = {255,40,65,115,85}; //Specify which brightness to use for each LED
 
 void setup() {
   //Loop through pins to set them as output and on
